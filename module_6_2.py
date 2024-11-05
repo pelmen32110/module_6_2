7489198ae38f97b07c5e92ea5c804263e025ee34
pelmen32110 <44937124+pelmen32110@users.noreply.github.com>
2024-11-06 02:56:30 +0700

    module_6_2.py

--- module_6_2.py
+++ module_6_2.py
@@ -9,13 +9,13 @@ class Vehicle:
 
 
     def get_model(self):
-        return print(f'Модель {self._Vehicle__model}')
+        return print(f'Модель {self.__model}')
 
     def get_horsepower(self):
-        return print(f'Мощность двигателя {self._Vehicle__engine_power}')
+        return print(f'Мощность двигателя {self.__engine_power}')
 
     def get_color(self):
-        return print(f'Цвет {self._Vehicle__color}')
+        return print(f'Цвет {self.__color}')
 
     def print_info(self):
         self.get_model()
@@ -24,8 +24,8 @@ class Vehicle:
         print(f'Владелец {self.owner}')
 
     def set_color(self,new_color):
-        if new_color.lower() in self._Vehicle__COLOR_VARIANTS:
-            self._Vehicle__color = new_color
+        if new_color.lower() in self.__COLOR_VARIANTS:
+            self.__color = new_color
         else:
             print('Нельзя сменить цвет на ', new_color)
 
@@ -39,6 +39,12 @@ vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
 vehicle1.print_info()
 
 # Меняем свойства (в т.ч. вызывая методы)
+vehicle1.set_color('Pink')
+vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
+
+# Проверяем что поменялось
+vehicle1.print_info()
 vehicle1.set_color('Pink')
 vehicle1.set_color('BLACK')
 vehicle1.owner = 'Vasyok'
